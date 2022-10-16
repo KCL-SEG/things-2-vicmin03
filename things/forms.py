bc0a2a3305829f37c6776729210940526dd0cb0f
Victoria Mac <vicmin03@outlook.com>
2022-10-16 15:18:59 +0100

    Change form to use Thing model

--- things/forms.py
+++ things/forms.py
@@ -1,8 +1,15 @@
 """Forms of the project."""
 
 from django import forms
+from .models import Thing
 
-class ThingsForm(forms.Form):
-    name = forms.CharField(label="Name:", max_length=30)
-    description = forms.CharField(label="Description:", max_length=400, widget=forms.Textarea)
-    quantity = forms.IntegerField(label="Quantity:", widget=forms.NumberInput())
+class ThingsForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description': forms.Textarea, 'quantity':forms.NumberInput()}
+
+
+    # name = forms.CharField(label="Name:", max_length=30)
+    # description = forms.CharField(label="Description:", max_length=400, widget=forms.Textarea)
+    # quantity = forms.IntegerField(label="Quantity:", widget=forms.NumberInput())
