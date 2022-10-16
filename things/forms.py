38e951f473f6cef9dfc3c80b5b3b25e4018df51c
Victoria Mac <vicmin03@outlook.com>
2022-10-16 15:01:39 +0100

    Add thingsform for name, description and quantity input
    Create ThingsForm for input
    Add ThingsForm to appear on home page

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,8 @@
 """Forms of the project."""
 
-# Create your forms here.
+from django import forms
+
+class ThingsForm(forms.Form):
+    name = forms.CharField(label="Name:", max_length=30)
+    description = forms.CharField(label="Description:", max_length=400, widget=forms.Textarea)
+    quantity = forms.IntegerField(label="Quantity:", widget=forms.NumberInput())
--- things/views.py
+++ things/views.py
@@ -1,4 +1,6 @@
+from .forms import ThingsForm
 from django.shortcuts import render
 
 def home(request):
+    form = ThingsForm()
     return render(request, 'home.html', {'form': form})
